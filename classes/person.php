9a1b78badace1ffaa56876b6707df3ff4aea2400
César Mora Cid <cesar.mcid@gmail.com>
2019-11-14 21:12:58 -0300

    Transformando estructuras de datos hibridas a estructuras de datos

--- classes/person.php
+++ classes/person.php
@@ -1,6 +1,5 @@
 <?php 
 class local_social_course_person{
-
   public $id;
   public $username;
   public $firstname;
@@ -8,16 +7,4 @@ class local_social_course_person{
   public $email;
   public $lastaccess;
   public $lastaction;
-
-  public function get(){
-    $author = new stdClass();
-    $author->id = $this->id;
-    $author->username = $this->username;
-    $author->firstname = $this->firstname;
-    $author->lastname = $this->lastname;
-    $author->email = $this->email;
-    $author->lastaccess = $this->lastaccess;
-    $author->lastaction = $this->lastaction;
-    return $author;
-  }
 }
--- classes/share.php
+++ classes/share.php
@@ -1,17 +1,7 @@
 <?php 
 class local_social_course_share{
-
   public $name;
   public $groupid;
   public $roleid;
   public $type;
-
-  public function get(){
-    $shared = new stdClass();
-    $shared->name = $this->name;
-    $shared->type = $this->type;
-    $shared->groupid = $this->groupid;
-    $shared->roleid = $this->roleid;
-    return $shared;
-  }
 }
