4c614e4036c65ffcdf381ab60690182190430f5a
César Mora Cid <cesar.mcid@gmail.com>
2020-01-04 18:55:17 -0300

    Avance refactorización clase core-participant

--- classes/core.php
+++ classes/core.php
@@ -1,190 +1,221 @@
-<?php 
-  class local_social_course_core{
-    const  USER_FIELDS = ["id", "username", "firstname", "lastname", "email"];
+<?php
+class local_social_course_core
+{
+  const  USER_FIELDS = ["id", "username", "firstname", "lastname", "email"];
 
-    protected function get_course_from_id($courseid, $strictness = MUST_EXIST){
-      global $DB;
-      $course = $DB->get_record("course", array("id" => $courseid), '*', $strictness);
-      return $course;
-    }
-
-    protected function get_user_from_id($userid, $strictness = MUST_EXIST){
-      global $DB;
-      $user = $DB->get_record("user", array("id" => $userid), '*', $strictness);
-      return $user;
-    }
-
-    protected function gettable_user_fields($prefix = null, $renameid = null){
-      $query = "";
-      $fields = self::USER_FIELDS;
-      $last = count($fields) - 1;
-      foreach($fields as $iteration => $field){
-        if($iteration == 0 && $renameid){
-          $query .= "$prefix$renameid";
-        }else{
-          $query .= "$prefix$field";
-        }
-        if($iteration < $last){
-          $query .= ", ";
-        }
-      }
-      return $query;
-    }
-
-    protected function clean_user_properties($user){
-      $cleaned = new stdClass();
-      if(gettype($user) != 'object'){
-        print_error("clean_user_properties only accept a user object, not " . gettype($user));
-      }
-      foreach(self::USER_FIELDS as $property){
-        $cleaned->$property = isset($user->$property) ? $user->$property : null;
-      }
-      return $cleaned;
-    }
-
-    protected function clean_users_properties($users){
-      if(gettype($users) != 'array'){
-        print_error("clean_users_properties only accept a array, not " . gettype($users));
-      }
-      $cleaned = array();
-      foreach($users as $user){
-        $user = self::clean_user_properties($user);
-        array_push($cleaned, $user);
-      }
-      return $cleaned;
-    }
-
-    protected function picture_profile_url($userid){
-      $url = new moodle_url("/user/pix.php/$userid/f1.jpg");
-      return $url->out(false);
-    }
-
-    // const  USER_FIELDS = "u.id, u.username, u.firstname, u.lastname, u.email, u.lastaccess";
-    // protected $course;
-    // protected $context;
-    // protected $user;
-    // protected $groups;
-    // protected $enrolled;
-    // protected $roles;
-
-    // public function __construct($courseid, $userid){
-    //   $this->course = self::get_course_from_id($courseid);
-    //   $this->user = self::get_user_from_id($userid);
-    //   $this->context = context_course::instance($this->course->id);
-    //   $this->groups = groups_get_all_groups($this->course->id);
-    //   self::set_enrolled_users();
-    //   self::set_roles();
-    // }
-
-    // public function get_course_from_id($courseid){
-    //   global $DB;
-    //   $course = $DB->get_record("course", array("id" => $courseid), '*', MUST_EXIST);
-    //   return $course;
-    // }
-
-    // public function get_user_from_id($userid){
-    //   global $DB;
-    //   $user = $DB->get_record("user", array("id" => $userid), '*', MUST_EXIST);
-    //   $user->roles = [];
-    //   return $user;
-    // }
-
-    // public function get_availables_roles(){
-    //   return $this->roles;
-    // }
-
-    // public function get_groups(){
-    //   return array_values($this->groups);
-    // }
-
-    // public function get_enrolled(){
-    //   return array_values($this->enrolled);
-    // }
-
-    // private function set_enrolled_users(){
-    //   $groups = self::get_groups_with_members();
-    //   $users = get_enrolled_users($this->context, '', 0, self::USER_FIELDS);
-    //   foreach($users as $userid => $user){
-    //     $user->pictureurl = self::get_picture_url($user->id);
-    //     $user->roles = [];
-    //     $user->groups = self::find_user_groups($groups, $user->id);
-    //   }
-    //   $this->enrolled = $users;
-    // }
-    
-    // public function get_groups_with_members(){
-    //   $groups = array();
-    //   foreach($this->groups as $group){
-    //     $group = clone $group;
-    //     $group->members = groups_get_members($group->id, self::USER_FIELDS);
-    //     array_push($groups, $group);
-    //   }
-    //   return $groups;
-    // }
-
-    // private function find_user_groups($groups, $memberid){
-    //   $user_groups = array();
-    //   foreach($groups as $groupid => $group){
-    //     foreach($group->members as $member){
-    //       if($member->id == $memberid){
-    //         $group_copy = clone $group;
-    //         unset($group_copy->members);
-    //         array_push($user_groups, $group_copy);
-    //         continue;
-    //       }
-    //     }
-    //   }
-    //   return $user_groups;
-    // }
-
-    // private function get_picture_url($userid){
-    //   $url = new moodle_url("/user/pix.php/$userid/f1.jpg");
-    //   return $url->out(false);
-    // }
-
-    // public function set_roles(){
-    //   $this->roles = [];
-    //   $roles = new stdClass();
-    //   $roles->existing = self::get_existing_roles(); 
-    //   $roles->available = [];
-    //   foreach($roles->existing as $role){
-    //     $users = get_role_users($role->id, $this->context);
-    //     $users = array_values($users);
-    //     if(count($users) > 0){
-    //       self::add_role($role);
-    //       foreach($users as $user){
-    //         array_push($this->enrolled[$user->id]->roles, $role);
-    //         if($user->id == $this->user->id){
-    //           array_push($this->user->roles, $role);
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
-
-    // private function add_role($role){
-    //   $is_setted = self::role_is_set($role);
-    //   if(!$is_setted){
-    //     array_push($this->roles, $role);
-    //   }
-    // }
-
-    // private function role_is_set($role){
-    //   $is_setted = false;
-    //   foreach($this->roles as $setted_role){
-    //     if($role->id == $setted_role->id){
-    //       $is_setted = true;
-    //       break;
-    //     }
-    //   }
-    //   return $is_setted;
-    // }
-
-    // private function get_existing_roles(){
-    //   global $DB;
-    //   $sql = "select * from {role}";
-    //   $rows = $DB->get_records_sql($sql, array());
-    //   $rows = array_values($rows);
-    //   return $rows;
-    // }
+  protected function get_course_from_id($courseid, $strictness = MUST_EXIST)
+  {
+    global $DB;
+    $course = $DB->get_record("course", array("id" => $courseid), '*', $strictness);
+    return $course;
   }
+
+  protected function get_user_from_id($userid, $strictness = MUST_EXIST)
+  {
+    global $DB;
+    $user = $DB->get_record("user", array("id" => $userid), '*', $strictness);
+    return $user;
+  }
+
+  protected function sql_query_user_fields ($prefix = null, $renameid = null)
+  {
+    $query = "";
+    $fields = self::USER_FIELDS;
+    $last = count($fields) - 1;
+    foreach ($fields as $iteration => $field) {
+      if ($iteration == 0 && $renameid) {
+        $query .= "$prefix$renameid";
+      } else {
+        $query .= "$prefix$field";
+      }
+      if ($iteration < $last) {
+        $query .= ", ";
+      }
+    }
+    return $query;
+  }
+
+  protected function clean_user_properties($user)
+  {
+    $cleaned = new stdClass();
+    if (gettype($user) != 'object') {
+      print_error("clean_user_properties only accept a user object, not " . gettype($user));
+    }
+    foreach (self::USER_FIELDS as $property) {
+      $cleaned->$property = isset($user->$property) ? $user->$property : null;
+    }
+    return $cleaned;
+  }
+
+  protected function clean_users_properties($users)
+  {
+    if (gettype($users) != 'array') {
+      print_error("clean_users_properties only accept a array, not " . gettype($users));
+    }
+    $cleaned = array();
+    foreach ($users as $user) {
+      $user = self::clean_user_properties($user);
+      array_push($cleaned, $user);
+    }
+    return $cleaned;
+  }
+
+  protected function picture_profile_url($userid)
+  {
+    $url = new moodle_url("/user/pix.php/$userid/f1.jpg");
+    return $url->out(false);
+  }
+
+  protected function all_platform_roles()
+  {
+    global $DB;
+    $sql = "select * from {role}";
+    $rows = $DB->get_records_sql($sql, array());
+    $roles = array_values($rows);
+    $roles = self::arrayCopy($roles);
+    return $rows;
+  }
+
+  function arrayCopy(array $array){
+    $result = array();
+    foreach ($array as $key => $val) {
+      if (is_array($val)) {
+        $result[$key] = self::arrayCopy($val);
+      } elseif (is_object($val)) {
+        $result[$key] = clone $val;
+      } else {
+        $result[$key] = $val;
+      }
+    }
+    return $result;
+  }
+
+  // const  USER_FIELDS = "u.id, u.username, u.firstname, u.lastname, u.email, u.lastaccess";
+  // protected $course;
+  // protected $context;
+  // protected $user;
+  // protected $groups;
+  // protected $enrolled;
+  // protected $roles;
+
+  // public function __construct($courseid, $userid){
+  //   $this->course = self::get_course_from_id($courseid);
+  //   $this->user = self::get_user_from_id($userid);
+  //   $this->context = context_course::instance($this->course->id);
+  //   $this->groups = groups_get_all_groups($this->course->id);
+  //   self::set_enrolled_users();
+  //   self::set_roles();
+  // }
+
+  // public function get_course_from_id($courseid){
+  //   global $DB;
+  //   $course = $DB->get_record("course", array("id" => $courseid), '*', MUST_EXIST);
+  //   return $course;
+  // }
+
+  // public function get_user_from_id($userid){
+  //   global $DB;
+  //   $user = $DB->get_record("user", array("id" => $userid), '*', MUST_EXIST);
+  //   $user->roles = [];
+  //   return $user;
+  // }
+
+  // public function get_availables_roles(){
+  //   return $this->roles;
+  // }
+
+  // public function get_groups(){
+  //   return array_values($this->groups);
+  // }
+
+  // public function get_enrolled(){
+  //   return array_values($this->enrolled);
+  // }
+
+  // private function set_enrolled_users(){
+  //   $groups = self::get_groups_with_members();
+  //   $users = get_enrolled_users($this->context, '', 0, self::USER_FIELDS);
+  //   foreach($users as $userid => $user){
+  //     $user->pictureurl = self::get_picture_url($user->id);
+  //     $user->roles = [];
+  //     $user->groups = self::find_user_groups($groups, $user->id);
+  //   }
+  //   $this->enrolled = $users;
+  // }
+
+  // public function get_groups_with_members(){
+  //   $groups = array();
+  //   foreach($this->groups as $group){
+  //     $group = clone $group;
+  //     $group->members = groups_get_members($group->id, self::USER_FIELDS);
+  //     array_push($groups, $group);
+  //   }
+  //   return $groups;
+  // }
+
+  // private function find_user_groups($groups, $memberid){
+  //   $user_groups = array();
+  //   foreach($groups as $groupid => $group){
+  //     foreach($group->members as $member){
+  //       if($member->id == $memberid){
+  //         $group_copy = clone $group;
+  //         unset($group_copy->members);
+  //         array_push($user_groups, $group_copy);
+  //         continue;
+  //       }
+  //     }
+  //   }
+  //   return $user_groups;
+  // }
+
+  // private function get_picture_url($userid){
+  //   $url = new moodle_url("/user/pix.php/$userid/f1.jpg");
+  //   return $url->out(false);
+  // }
+
+  // public function set_roles(){
+  //   $this->roles = [];
+  //   $roles = new stdClass();
+  //   $roles->existing = self::get_existing_roles(); 
+  //   $roles->available = [];
+  //   foreach($roles->existing as $role){
+  //     $users = get_role_users($role->id, $this->context);
+  //     $users = array_values($users);
+  //     if(count($users) > 0){
+  //       self::add_role($role);
+  //       foreach($users as $user){
+  //         array_push($this->enrolled[$user->id]->roles, $role);
+  //         if($user->id == $this->user->id){
+  //           array_push($this->user->roles, $role);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // private function add_role($role){
+  //   $is_setted = self::role_is_set($role);
+  //   if(!$is_setted){
+  //     array_push($this->roles, $role);
+  //   }
+  // }
+
+  // private function role_is_set($role){
+  //   $is_setted = false;
+  //   foreach($this->roles as $setted_role){
+  //     if($role->id == $setted_role->id){
+  //       $is_setted = true;
+  //       break;
+  //     }
+  //   }
+  //   return $is_setted;
+  // }
+
+  // private function get_existing_roles(){
+  //   global $DB;
+  //   $sql = "select * from {role}";
+  //   $rows = $DB->get_records_sql($sql, array());
+  //   $rows = array_values($rows);
+  //   return $rows;
+  // }
+}
--- classes/participant.php
+++ classes/participant.php
@@ -3,8 +3,6 @@
     protected $course;
     protected $user;
     protected $context;
-    protected $enrolled;
-    protected $roles;
 
     public function __construct($userid, $courseid){
       $this->classname = "local_social_course_participant";
@@ -12,90 +10,87 @@
       $this->course = self::get_course_from_id($courseid);
       $this->user = self::get_user_from_id($courseid);
       $this->context = context_course::instance($this->course->id);
-      $this->enrolled = self::enrolled();
-      $this->roles = self::roles_used();
-      dd($this->enrolled);
     }
     
     public function all_groups(){
       $groups = groups_get_all_groups($this->course->id);
-      $groups = array_values($groups);
+      $groups = self::arrayCopy($groups);
       return $groups;
-    }
-
-    private function enrolled(){
-      $prefix = "u.";
-      $users = get_enrolled_users($this->context, '', 0, self::gettable_user_fields($prefix));
-      foreach($users as $userid => $user){
-        $user->pictureurl = self::picture_profile_url($user->id);
-        $user->roles = [];
-        $user->groups = self::groups_assigned($user->id);
-      }
-      return $users;
-    }
-
-    private function groups_assigned($userid){
-      $groups =  self::all_groups_with_members();
-      $assigned = array();
-      foreach($groups as $group){
-        foreach($group->members as $member){
-          if($member->id == $userid){
-            unset($group->members);
-            array_push($assigned, $group);
-            break;
-          }
-        }
-      }
-      return $assigned;
     }
 
     public function all_groups_with_members(){
       $groups = array();
       foreach(self::all_groups() as $group){
         $prefix = "u.";
-        $group->members = groups_get_members($group->id, self::gettable_user_fields($prefix));
-        array_push($groups, $group);
+        $group->members = groups_get_members($group->id, self::sql_query_user_fields($prefix));
+        $groups[$group->id] = $group; 
       }
       return $groups;
     }
 
-    private function roles_used(){
-      $this->roles = [];
-      $roles = self::all_roles(); 
-      foreach($roles as $role){
-        $users = get_role_users($role->id, $this->context);
-        $users = array_values($users);
-        if(!empty($users)){
-          $role->users = self::clean_users_properties($users);
-          self::add_role($role);
-        }
-      }
-      return $this->roles;
+    public function enrolled(){
+      $prefix = "u.";
+      $users = get_enrolled_users($this->context, '', 0, self::sql_query_user_fields($prefix));
+      $users = array_values($users);
+      $users = self::add_profile_pictures($users);
+      $users = self::add_groups($users);
+      $users = self::add_roles($users);
+      return $users;
     }
     
-    private function all_roles(){
-      global $DB;
-      $sql = "select * from {role}";
-      $rows = $DB->get_records_sql($sql, array());
-      $rows = array_values($rows);
-      return $rows;
-    }
-
-    private function add_role($role){
-      $is_setted = self::role_was_added($role);
-      if(!$is_setted){
-        array_push($this->roles, $role);
+    private function add_profile_pictures($users){
+      foreach($users as $user){
+        $user->pictureurl = self::picture_profile_url($user->id);
       }
+      return $users;
     }
 
-    private function role_was_added($role){
-      $added = false;
-      foreach($this->roles as $setted_role){
-        if($role->id == $setted_role->id){
-          $added = true;
-          break;
+    private function add_groups($users){
+      $groups = new stdClass();
+      $groups->all = self::all_groups();
+      $groups->with_members = self::all_groups_with_members();
+      foreach($users as $user){
+        $user->groups = array();
+        foreach($groups->with_members as $group){
+          if(isset($group->members[$user->id])){
+            array_push($user->groups, $groups->all[$group->id]);
+          }
         }
       }
-      return $added;
+      return $users;
     }
+
+    private function add_roles($users){
+      $roles = self::all_platform_roles(); 
+      foreach($users as $user){
+        $user->roles = array();
+        foreach($roles as $role){
+          $role_users = get_role_users($role->id, $this->context);
+          if(!empty($role_users) && isset($role_users[$user->id])){
+            array_push($user->roles, $role);
+          }
+        }  
+      }
+      return $users;
+    }
+    
+    
+
+    // private function add_role($role){
+    //   $is_setted = self::role_was_added($role);
+    //   if(!$is_setted){
+    //     array_push($this->roles, $role);
+    //   }
+    // }
+
+    // private function role_was_added($role){
+    //   $added = false;
+    //   foreach($this->roles as $setted_role){
+    //     if($role->id == $setted_role->id){
+    //       $added = true;
+    //       break;
+    //     }
+    //   }
+    //   return $added;
+    // }
   }
